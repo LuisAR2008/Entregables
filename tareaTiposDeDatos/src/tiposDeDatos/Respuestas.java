700676eb1dd89ddce159212e022f4a2d14378bbd
LuisAraiza <luism.araiza@hotmail.com>
2023-06-21 20:20:31 -0600

    tareaTiposDeDatos

---
+++ tareaTiposDeDatos/src/tiposDeDatos/Respuestas.java
@@ -0,0 +1,20 @@
+package tiposDeDatos;
+
+
+public class Respuestas {
+
+	public static void main(String[] args) {
+	
+		tiposDatos result = new tiposDatos();
+		result.producto();
+		
+		Operaciones result1 = new Operaciones();
+	    result1.significado();
+	    
+	    Division result2 = new Division ();
+	    result2.division();
+
+
+	}
+
+}
