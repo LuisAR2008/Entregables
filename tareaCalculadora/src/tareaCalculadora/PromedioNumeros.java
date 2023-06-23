4488af8e19c8356822da9a16085e6bd5bdc271d7
LuisAraiza <luism.araiza@hotmail.com>
2023-06-23 14:01:26 -0600

    tareaCalculadora

--- tareaCalculadora/src/tareaCalculadora/PromedioNumeros.java
+++ tareaCalculadora/src/tareaCalculadora/PromedioNumeros.java
@@ -5,25 +5,38 @@ import java.util.Scanner;
 //Cálculo del promedio de números flotantes usando clases
 public class PromedioNumeros {
 
- public static double promedioNumero (double numero1 , double numero2) {
+ public static double promedioNumero (double numeros[]) {
     	 
-    	 double resultado= (numero1+numero2)/2;
-    	 return resultado;
+	  if (numeros.length == 0) {
+          return 0;   
+      }
+
+      double total = 0;
+      for (double numero : numeros) {
+          total += numero;
+      }
+      double promedio = total / numeros.length;
+      return promedio;
      }
 	
 	public static void main(String[] args) {
 		
+		int cantidadNumeros;
 		
 		Scanner entrada = new Scanner(System.in);
-		System.out.println("Ingresa Numero1");
-		double numero1 = entrada.nextDouble();
+		System.out.println("Ingresa la cantidad de Numeros a calcular ");
+		cantidadNumeros = entrada.nextInt();
 		
-		System.out.println("Ingresa Numero2");
-		double numero2 = entrada.nextDouble();
+		 double[] numeros = new double[cantidadNumeros];
+	       
+		 for (int i = 0; i < cantidadNumeros; i++) {
+	            System.out.print("Ingrese el número " + (i + 1) +":\n");
+	            numeros[i] = entrada.nextDouble();
+	        }
 		
-		double promedio = promedioNumero(numero1, numero2);
-		String resultado = String.format("%.2f",promedio);
-		System.out.println("El promedio de los numeros es: "+resultado);
+		 double resultado =(float) promedioNumero(numeros);
+		 String resultado1 = String.format("%.2f",resultado);
+	     System.out.println("El promedio es: " + resultado1);
 		
 		entrada.close();
 	}
